47157de7885a34d4eb52e157bbc39fe042b02592
Agostinorusso <agostino.russo04@gmail.com>
2025-04-18 20:30:39 +0200

    hh

--- Challenge3Project/ContentView.swift
+++ Challenge3Project/ContentView.swift
@@ -2,54 +2,17 @@
 //  ContentView.swift
 //  Challenge3Project
 //
-//  Created by Agostino Russo  on 10/12/24.
+//  Created by Agostino Russo  on 18/04/25.
 //
 
-import SwiftUICore
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject private var viewModel = ProgrammingLanguagesViewModel()
-
     var body: some View {
-        NavigationView {
-            VStack {
-                // Title
-                Text("DevEnc")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(.top, 20)
-                    .accessibilityLabel("Dev Dictionary")
-                    .accessibilityHint("A list of programming languages with descriptions and syntax")
-
-                // List of Programming Languages
-                List(viewModel.programmingLanguages) { language in
-                    NavigationLink(
-                        destination: ProgrammingLanguageDetailView(language: language, viewModel: viewModel)
-                            .accessibilityLabel("Go to details for \(language.name)")) {
-                        HStack {
-                            // Icon on the left
-                            Image(language.icon)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 70, height: 80) // Adjust size as needed
-                                .padding(.trailing, 10)
-                                .accessibilityLabel("\(language.name) icon")
-                            
-                            // Language Name
-                            Text(language.name)
-                                .font(.headline)
-                                .accessibilityLabel(language.name)
-                        }
-                    }
-                }
-                .listStyle(PlainListStyle())
-            }
-            .navigationTitle("")
-            .navigationBarHidden(true)
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
+
 #Preview {
     ContentView()
 }
---
+++ Challenge3Project/ProgrammingLanguage/SectionView.swift
@@ -0,0 +1,21 @@
+//
+//  AccessibilitySection.swift
+//  Challenge3Project
+//
+//  Created by Agostino Russo  on 18/04/25.
+//
+
+import SwiftUI
+
+struct AccessibilitySection: View {
+    var body: some View {
+        // Content of Accessibility Section
+        Text("This is the Accessibility Section.")
+    }
+}
+
+struct AccessibilitySection_Previews: PreviewProvider {
+    static var previews: some View {
+        AccessibilitySection()
+    }
+}
